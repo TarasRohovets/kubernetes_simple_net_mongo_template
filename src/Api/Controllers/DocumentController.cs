e711c5319e487554357461c0f566b8a1fd3a0619
Taras <rohovets.taras@gmail.com>
2022-08-07 13:33:47 +0200

    minimum api setup

---
+++ src/Api/Controllers/DocumentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        public DocumentController()
+        {
+
+        }
+
+        [HttpGet("getDocument")]
+        public async Task<IActionResult> GetDocument()
+        {
+            return Ok();
+        }
+
+        [HttpPost("postDocument")]
+        public async Task<IActionResult> PostDocument()
+        {
+            return Ok();
+        }
+    }
+}
